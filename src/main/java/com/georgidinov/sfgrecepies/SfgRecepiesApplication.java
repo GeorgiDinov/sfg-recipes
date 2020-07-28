8951422b8de836e2d42c6387818c76445ed0bb97
Georgi Dinov <georgidinov1@abv.bg>
2020-07-28 08:14:35 +0300

    Initial Commit

---
+++ src/main/java/com/georgidinov/sfgrecepies/SfgRecepiesApplication.java
@@ -0,0 +1,13 @@
+package com.georgidinov.sfgrecepies;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SfgRecepiesApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SfgRecepiesApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/georgidinov/sfgrecepies/SfgRecepiesApplicationTests.java
@@ -0,0 +1,13 @@
+package com.georgidinov.sfgrecepies;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SfgRecepiesApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
