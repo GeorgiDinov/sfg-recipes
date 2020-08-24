a627d4d70ffc085dbeb6e4f840687d7fc162ad48
Georgi Dinov <georgidinov1@abv.bg>
2020-08-24 21:14:45 +0300

    Added form to create or update recipe, added additional two methods in the RecipeController class.

--- src/main/java/com/georgidinov/recipesapp/controllers/RecipeController.java
+++ src/main/java/com/georgidinov/recipesapp/controllers/RecipeController.java
@@ -1,11 +1,14 @@
 package com.georgidinov.recipesapp.controllers;
 
+import com.georgidinov.recipesapp.commands.RecipeCommand;
 import com.georgidinov.recipesapp.services.RecipeService;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 @Slf4j
@@ -28,5 +31,17 @@ public class RecipeController {
         model.addAttribute("recipe", this.recipeService.findById(Long.valueOf(id)));
         return "recipes/show";
     }//end of method recipes
+
+    @RequestMapping("recipe/new")
+    public String newRecipe(Model model) {
+        model.addAttribute("recipe", new RecipeCommand());
+        return "recipes/recipeform";
+    }
+
+    @PostMapping("recipe")
+    public String saveOrUpdate(@ModelAttribute RecipeCommand command) {
+        RecipeCommand savedCommand = this.recipeService.saveRecipeCommand(command);
+        return "redirect:/recipe/show/" + savedCommand.getId();
+    }
 
 }//end of class RecipeController
